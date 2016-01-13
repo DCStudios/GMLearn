78605c779393046451be0f80a6363a5f1b852e2e
DCStudios <super_cedrik@hotmail.com>
2016-01-12 23:06:40 -0500

    Added exercice page

---
+++ lessons/01_variables/exercice_1.php
@@ -0,0 +1,13 @@
+<div id="member-page-container">
+	<div id="member-page" class="nextShortTransition animFadeInLeftAlt transition-exclude">
+
+		<div class="message">
+			<h1 class="message-header">Not available yet</h1>
+			<p class="message-body">
+				The exercice you are trying to view is currently under construction.<be>
+				Please visit back in the future to complete it!
+			</p>
+		</div>
+
+	</div>
+</div>
